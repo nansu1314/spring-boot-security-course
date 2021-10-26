899902ca0fc84c18f07d7e31a58cb3f643446d34
Su, Chen <>
2021-10-26 16:25:03 +0800

    code refactor

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -7,17 +7,25 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.security.ApplicationUserRole.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.example.demo.security.ApplicationUserRole.ADMIN;
+import static com.example.demo.security.ApplicationUserRole.ADMINTRAINEE;
+import static com.example.demo.security.ApplicationUserRole.STUDENT;
 
 
 @Configuration
 @EnableWebSecurity
+//EnableGlobalMethodSecurity  添加了aop,拦截@PreAuthorize
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -51,29 +59,48 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .roles(STUDENT.name()) // ROLE_STUDENT
                 .authorities(STUDENT.getGrantedAuthorities())
                 .build();
 
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .roles(ADMIN.name()) // ROLE_ADMIN
                 .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
                 .authorities(ADMINTRAINEE.getGrantedAuthorities())
                 .build();
 
-        return new InMemoryUserDetailsManager(
-                annaSmithUser,
-                lindaUser,
-                tomUser
-        );
+        Collection<UserDetails> users = Arrays.asList(annaSmithUser, lindaUser, tomUser);
+//        users.forEach((user) -> {
+//            assignUserPermission(user);
+//        });
 
+        return new InMemoryUserDetailsManager(users);
+    }
+
+    private void assignUserPermission(UserDetails user) {
+        if (user == null) {
+            return;
+        }
+        Collection<GrantedAuthority> authorities = (Collection<GrantedAuthority>) user.getAuthorities();
+
+        Collection<GrantedAuthority> newAuthorities = new ArrayList<>();
+        if (authorities != null) {
+            authorities.stream().forEach((authority) -> {
+                if (authority.getAuthority().startsWith("ROLE_")) {
+                    String role = authority.getAuthority().substring(5);
+                    newAuthorities.addAll(ApplicationUserRole.valueOf(role).getGrantedAuthorities());
+                }
+            });
+        }
+
+        authorities.addAll(newAuthorities);
     }
 }
