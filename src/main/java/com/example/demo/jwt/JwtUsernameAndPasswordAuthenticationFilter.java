8537b7507213d1071362a8b409df6d98ea4bc7a4
Su, Chen <>
2021-11-24 09:50:56 +0800

    code refinement

--- src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -20,39 +20,39 @@ import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    private final AuthenticationManager authenticationManager;
+
     private final JwtConfig jwtConfig;
     private final SecretKey secretKey;
 
     public JwtUsernameAndPasswordAuthenticationFilter(AuthenticationManager authenticationManager,
                                                       JwtConfig jwtConfig,
                                                       SecretKey secretKey) {
-        this.authenticationManager = authenticationManager;
+        this.setAuthenticationManager(authenticationManager);
         this.jwtConfig = jwtConfig;
         this.secretKey = secretKey;
     }
 
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-
-        try {
-            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
-                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
-
-            Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    authenticationRequest.getUsername(),
-                    authenticationRequest.getPassword()
-            );
-
-            Authentication authenticate = authenticationManager.authenticate(authentication);
-            return authenticate;
-
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request,
+//                                                HttpServletResponse response) throws AuthenticationException {
+//
+//        try {
+//            UsernameAndPasswordAuthenticationRequest authenticationRequest = new ObjectMapper()
+//                    .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
+//
+//            Authentication authentication = new UsernamePasswordAuthenticationToken(
+//                    authenticationRequest.getUsername(),
+//                    authenticationRequest.getPassword()
+//            );
+//
+//            Authentication authenticate = authenticationManager.authenticate(authentication);
+//            return authenticate;
+//
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//    }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request,
